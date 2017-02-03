5b5244b4d99ac3dc544804ac4e28d73614b8015a
taroooyan <taroooyan@gmail.com>
2017-02-03 23:54:51 +0900

    add hub cookbooks

--- recipe.rb
+++ recipe.rb
@@ -2,10 +2,11 @@
 include_recipe './cookbooks/vim/default.rb'
 include_recipe './cookbooks/zsh/default.rb'
 include_recipe './cookbooks/git/default.rb'
+include_recipe './cookbooks/hub/default.rb'
 include_recipe './cookbooks/gibo/default.rb'
 include_recipe './cookbooks/tmux/default.rb'
 include_recipe './cookbooks/the-silver-searcher/default.rb'
-
+return
 # dotfiles setup
 include_recipe './cookbooks/dotfiles/default.rb'
 
